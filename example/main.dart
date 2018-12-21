7e3bae0d2f0be51ab8140fefff6bcefe53835114
Israel Lugo <hostelixisrael@gmail.com>
2018-12-21 12:16:12 -0400

    [ADD] add example file

---
+++ example/main.dart
@@ -0,0 +1,9 @@
+import 'package:mercadopago_sdk/mercadopago_sdk.dart';
+
+void main() async {
+  var mp = MP('CLIENT_ID', 'CLIENT_SECRET');
+
+  String token = await mp.getAccessToken();
+
+  print('Mercadopago token ${token}');
+}
